4b0112d50278b51b6a2d20833d0e40b7dbd9512e
Menno Brandt <54868340+mennobrandt@users.noreply.github.com>
2022-04-25 13:42:57 +0930

    Almost function horizontal flipper

--- flipHorizontal.m
+++ flipHorizontal.m
@@ -1,4 +1,12 @@
-function [] = flipHorizontal(testInput)
+function [newImage] = flipHorizontal(tempImage)
     % Flips an image horizontally by reversing the columns
-    disp(testInput);
+    [oldRows,oldCols] = size(tempImage);
+    newImage = [];
+    for i = 1:oldRows
+        imageRow = [];
+        for j = 1:oldCols
+            imageRow = [tempImage(i,j),imageRow];
+        end
+        newImage = [newImage;imageRow];
+    end
 end
