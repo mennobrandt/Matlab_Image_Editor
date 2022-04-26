6cfe9bc8fe7c81b0e552bba44f84ca49a18f8637
Menno Brandt <54868340+mennobrandt@users.noreply.github.com>
2022-04-26 20:09:59 +0930

    Comments.

--- flipHorizontal.m
+++ flipHorizontal.m
@@ -1,12 +1,12 @@
 function [newImage] = flipHorizontal(tempImage)
     % Flips an image horizontally by reversing the columns
     [oldRows,oldCols] = size(tempImage);
-    newImage = [];
+    newImage = []; % Blank image matrix to be populated. 
     for i = 1:oldRows
         imageRow = [];
         for j = 1:oldCols
-            imageRow = [tempImage(i,j),imageRow];
+            imageRow = [tempImage(i,j),imageRow]; % Prepend the old row's entries into newRow. 
         end
-        newImage = [newImage;imageRow];
+        newImage = [newImage;imageRow]; % Populate newImage matrix with the reversed rows. 
     end
 end
