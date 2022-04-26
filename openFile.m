4214bf6cbe66cbdce66a1c37f421689bb7a3368d
Menno Brandt <54868340+mennobrandt@users.noreply.github.com>
2022-04-26 17:06:42 +0930

    Return less image info

--- openFile.m
+++ openFile.m
@@ -1,8 +1,8 @@
-function[fullImgPath,uploadedImg,imgName,numRows,numCols] = openFile()
+function[image,filepath,imageName] = openFile()
     % Uses MATLAB GUI to select an image file
-    [imgName,path] = uigetfile("*.jpg;*.png;*.tff","Select Image");
-    fullImgPath = fullfile(path, imgName); % Stitches the director and filename together
-
-    uploadedImg = imread(fullImgPath);
-    [numRows,numCols] = size(uploadedImg); % Assign image dimensions
+    [imageName,path] = uigetfile("*.jpg;*.png;*.tff","Select Image");
+    filepath = fullfile(path, imageName); % Stitches the director and filename together
+    fprintf("Selected File: %s\n",imageName);
+    
+    image = imread(filepath);
 end
