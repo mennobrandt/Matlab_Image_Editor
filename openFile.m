a501b24e6f8459ef82bc2eabc153990579a8e8bf
Menno Brandt <54868340+mennobrandt@users.noreply.github.com>
2022-04-26 20:07:15 +0930

    Added comments. Functionality is the same.

--- openFile.m
+++ openFile.m
@@ -1,8 +1,7 @@
 function[image,filepath,imageName] = openFile()
     % Uses MATLAB GUI to select an image file
-    [imageName,path] = uigetfile("*.jpg;*.png;*.tff","Select Image");
-    filepath = fullfile(path, imageName); % Stitches the director and filename together
-    fprintf("Selected File: %s\n",imageName);
-    
+    [imageName,path] = uigetfile("*.jpg;*.png;*.tff","Select Image"); % Open file browser
+    filepath = fullfile(path, imageName); % Combines the directory and filename together
+    fprintf("Selected File: %s\n",imageName); % Display name of chosen file. 
     image = imread(filepath);
 end
