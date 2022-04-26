0bf318cdc308df453bd3c5311a691c586c605238
Menno Brandt <54868340+mennobrandt@users.noreply.github.com>
2022-04-26 20:06:19 +0930

    Call new constructImage() function.

--- main.m
+++ main.m
@@ -1,7 +1,12 @@
-% MATLAB (TUI) Image Editor
+% MATLAB Image Editor by Menno Brandt (a1849852)
 clc;
-clear;
+clear all;
 
-[filePath,image,imageName] = openFile(); % Open image to be edited.
-commandList = qeueEdits(); % Read and qeue edits. 
-executeEdits(commandList,image); % Execute edits. 
+% Open the file browser, and let the user select an image. 
+[image,filepath,imageName] = openFile(); 
+% Read and qeue the edits the user wants to perform.
+commandList = qeueEdits();
+% Perform the edits, through calling the necessary functions. 
+editedImage = executeEdits(commandList,image); 
+% When edits are done, overwrite the old image with the new one. 
+constructImage(editedImage,filepath,imageName);
