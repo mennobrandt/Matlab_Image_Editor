e23e361db3b66f269fbb1b09e16450db3003cc6c
Menno Brandt <54868340+mennobrandt@users.noreply.github.com>
2022-04-26 17:10:10 +0930

    Invert colours (not functional yet)

---
+++ invert.m
@@ -0,0 +1,11 @@
+function[newImage] = invert(tempImage)
+    % Invert an image's colours. (Black becomes white, white becomes black)
+    [oldRows,oldCols] = size(tempImage);
+    newImage = [];
+
+    for i = 1:oldRows
+            for j = 1:oldCols
+
+            end
+    end
+end
