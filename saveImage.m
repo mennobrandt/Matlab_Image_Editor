f46cde5c32496704e0c2b32dfe4b63d907e3a46f
Menno Brandt <54868340+mennobrandt@users.noreply.github.com>
2022-04-26 20:45:24 +0930

    Saves Image and now works

--- saveImage.m
+++ saveImage.m
@@ -1,6 +1,5 @@
-function[] = saveImage(newImage,filepath,imageName)
-    % Saves the image, and overwrites the old copy. 
-    imshow(newImage);
-    disp(filepath);
-    disp(imageName);
+function[] = saveImage(newImage,filepath)
+    % Saves the edited image, and overwrites the old copy. 
+    imwrite(newImage,filepath); % Save image data to same filepath. 
+    imshow(filepath); % Display the image. 
 end
